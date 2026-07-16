4d3dd95d19592b4657732c1a371a8e359ec6ee50
Martin Ahindura <sales@sopherapps.com>
2026-07-16 22:45:30 +0200

    qpi-driver: Provide important defaults for data-dir, ca-file etc in install-systemd.sh

--- qpi-driver/install-systemd.sh
+++ qpi-driver/install-systemd.sh
@@ -11,9 +11,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# The version of qpi-driver to install.
-# This should match the qpi-ui version if provided via environment variable.
-QPI_DRIVER_VERSION="${QPI_DRIVER_VERSION:-}"
 
 # Detect the real user who ran sudo
 if [ -n "$SUDO_USER" ]; then
@@ -28,12 +25,22 @@ echo "Installing for user: $REAL_USER (Home: $REAL_HOME)"
 echo ""
 
 # 1. Prompt for configuration if not provided via environment
-[ -z "$QPI_TOKEN" ] && read -p "Enter QPI Access Token: " QPI_TOKEN
-[ -z "$QPI_ADDR" ] && read -p "Enter QPI Server Address (e.g. https://qpi.sopherapps.se): " QPI_ADDR
-[ -z "$CA_FINGERPRINT" ] && read -p "Enter CA Fingerprint: " CA_FINGERPRINT
-[ -z "$QPU_NAME" ] && read -p "Enter QPU Name (e.g. rigetti-aspen-1): " QPU_NAME
+while [ -z "$QPI_TOKEN" ]; do read -p "Enter QPI Access Token: " QPI_TOKEN; done
+while [ -z "$QPI_ADDR" ]; do read -p "Enter QPI Server Address (e.g. https://qpi.sopherapps.se): " QPI_ADDR; done
+while [ -z "$CA_FINGERPRINT" ]; do read -p "Enter CA Fingerprint: " CA_FINGERPRINT; done
+while [ -z "$QPU_NAME" ]; do read -p "Enter QPU Name (e.g. rigetti-aspen-1): " QPU_NAME; done
+
+# Optional
 [ -z "$EXECUTOR" ] && read -p "Enter Executor (mock, qiskit_aer, quantify, qblox, presto) [mock]: " EXECUTOR
 EXECUTOR=${EXECUTOR:-mock}
+
+# The version of qpi-driver to install.
+# This should match the qpi-ui version if provided via environment variable.
+QPI_DRIVER_VERSION="${QPI_DRIVER_VERSION:-}"
+QPI_DATA_DIR="${QPI_DATA_DIR:-"/var/qpi-driver/${$QPU_NAME}"}"
+QPI_CA_FILE="${$QPI_CA_FILE:-"${QPI_DATA_DIR}/qpi.ca.pem"}"
+QPI_QUANTIFY_DEVICE_CONFIG="${$QPI_QUANTIFY_DEVICE_CONFIG:-"${QPI_DATA_DIR}/quantify.device.yml"}"
+QPI_QUANTIFY_HARDWARE_CONFIG="${$QPI_QUANTIFY_HARDWARE_CONFIG:-"${QPI_DATA_DIR}/quantify.hardware.json"}"
 
 # 2. Install uv if not present
 if ! sudo -u "$REAL_USER" command -v uv >/dev/null 2>&1; then
@@ -75,6 +82,10 @@ After=network.target
 Type=simple
 
 Environment="QPI_ACCESS_TOKEN=$QPI_TOKEN"
+Environment="QPI_DATA_DIR=$QPI_DATA_DIR"
+Environment="QPI_CA_FILE=$QPI_CA_FILE"
+Environment="QPI_QUANTIFY_DEVICE_CONFIG=$QPI_QUANTIFY_DEVICE_CONFIG"
+Environment="QPI_QUANTIFY_HARDWARE_CONFIG=$QPI_QUANTIFY_HARDWARE_CONFIG"
 # Standard Python output buffering disabled to ensure logs appear immediately in journalctl
 Environment=PYTHONUNBUFFERED=1
 
